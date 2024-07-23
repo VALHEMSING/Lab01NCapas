fa5b76ab612e85323e282fa18d968e7fa31fba04
Julian David Rivera Quintero <juli.rivera.quintero@gmail.com>
2024-07-23 15:52:14 -0500

    feat(back-end): added later BLL witch class 'Customer', a folder 'Exceptions'and into methods statics, finally added logic programing

---
+++ Orders/BLL/Customers.cs
@@ -0,0 +1,152 @@
+﻿using BLL.Exeptions;
+using DAL;
+using Entities.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.Metrics;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+namespace BLL
+{
+    public class Customers
+    {
+        /*-------------------------------------------------------------------------------------------------------------------------------*/
+        
+        //Tarea asincrona para crear al cliente si no existe y validacion
+        public async Task<Customer> CreateAsync(Customer customer)
+        {
+            Customer customerResult = null;
+            using (var repository = RepositoryFactory.CreateRepository())
+            {
+                //Buscar si el nombre del cliente existe
+                Customer customerSearch = await repository.RetrieveAsync<Customer>(c => c.FirstName == customer.FirstName);
+                if(customerSearch == null)
+                {
+                    //No existe, podemos crearlo
+                    customerResult = await repository.CreateAsync(customer);
+                }
+                else
+                {
+                    /*
+                     * Podriamos lanzar una excepcion
+                     * para modificar que el cliente ya existe.
+                     * podriamos incluso crear una capa de Excepciones
+                     * personalizada y consumirla desde otras capas.
+                    */
+                    CustomersExecptions.ThrowCustomerAlreadyExistException(customerSearch.FirstName, customerSearch.LastName);
+                }
+                
+            }
+            // Verificar si customerResult sigue siendo nulo y lanzar una excepción en caso afirmativo
+            if (customerResult == null)
+            {
+                throw new InvalidOperationException("El cliente no pudo ser creado.");
+            }
+            return customerResult;
+
+            
+        }
+
+        /*-------------------------------------------------------------------------------------------------------------------------------*/
+        
+        //Tarea asincrona para obtener todos los clientes
+        public async Task<List<Customer>> RetrieveAllAsync()
+        {
+            List<Customer> result = null;
+            using(var r = RepositoryFactory.CreateRepository())
+            {
+                /*
+                 * Define le criterio de filtro para obtener todolos los clientes
+                 */
+                Expression<Func<Customer, bool>> allCustomersCriteria = x => true;
+                result = await r.FilterAsync<Customer>(allCustomersCriteria);
+            }
+            return result;
+
+            //return result ?? new List<Customer>(); // Asegurarse de que no se retorne un valor nulo
+            
+            /*
+             * Al devolver result, utilizo el operador de coalescencia nula (??) para asegurar que si result es nulo, se devuelva una nueva
+             * lista vacía (new List<Customer>()).
+             */
+        }
+
+        /*-------------------------------------------------------------------------------------------------------------------------------*/
+        //Tara asincora de llamar al cliente por Id
+        public async Task <Customer> RetrieveByIDAsync(int id)
+        {
+            Customer result = null;
+            using (var repository = RepositoryFactory.CreateRepository())
+            {
+                Customer customer = await repository.RetrieveAsync<Customer>(c => c.Id == id);
+                //Checkeamos si el cliente fue encontrado
+                if (customer == null)
+                {
+                    //Lanzamos una excepcion en caso de no haber encontrado
+                    CustomersExecptions.ThrowInvalidCusrtomerDataException(id);
+                }
+
+                return result;
+            }
+        }
+
+
+        /*-------------------------------------------------------------------------------------------------------------------------------*/
+    
+        //Tarea asincrona para actualizar o modificar
+        public async Task<bool> UpdateAsync(Customer customer)
+        {
+            bool result = false;
+            using (var repository = RepositoryFactory.CreateRepository())
+            {
+                //Validar que el nombre no exista
+                Customer customerSearch = await repository.RetrieveAsync<Customer>(c => c.FirstName == c.FirstName && c.Id != customer.Id);
+                if( customerSearch == null)
+                {
+                    //No existe
+                    result = await repository.UpdateAsync(customer);
+                }
+                else
+                {
+                    /*
+                     * Podemos implementar alguna logica para
+                     * indicar que no se pudo modificar
+                     */
+                    CustomersExecptions.ThrowCustomerAlreadyExistException(customerSearch.FirstName, customerSearch.LastName);
+                }
+            }
+            return result;
+        }
+        /*-------------------------------------------------------------------------------------------------------------------------------*/
+        
+        //Tarea asincrona para eliminar
+        public async Task<bool> DeleteAsync(int id)
+        {
+            bool result = false;
+            //Buscar un cliente para ver si tiene Orders(Ordenes de compra)
+            var customer = await RetrieveByIDAsync(id);
+            if(customer != null)
+            {
+                //Eliminar al cliete
+                using (var repository = RepositoryFactory.CreateRepository())
+                {
+                    result = await repository.DeleteAsync(customer);
+                }
+            }
+            else
+            {
+                /*
+                 * Podemos implementar alguna logica para
+                 * indicar que el producto no existe
+                 */
+                CustomersExecptions.ThrowInvalididCustomerIdException(id);
+            }
+            return result;
+        }
+
+        /*-------------------------------------------------------------------------------------------------------------------------------*/
+
+    }
+}
---
+++ Orders/BLL/Exeptions/CustomersExecptions.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL.Exeptions
+{
+    public class CustomersExecptions : Exception
+    {
+        /// Agregar mas metodos estaticos
+        
+        //Constructor
+        private CustomersExecptions(string message) : base(message)
+        {
+            //Opcional: agregar constructo logico para logueo o manejo de errores del cliente
+        }
+        /*-------------------------------------------------------------------------------------------------------------------------------*/
+        public static void ThrowCustomerAlreadyExistException(string firstName, string lastName)
+        {
+            throw new CustomersExecptions($"A client with the name already exist {firstName} {lastName}");
+        }
+        /*-------------------------------------------------------------------------------------------------------------------------------*/
+        public static void ThrowInvalidCusrtomerDataException(int id)
+        {
+            throw new CustomersExecptions($"El ID del cliente '{id}' no es válido.");
+        }
+        /*-------------------------------------------------------------------------------------------------------------------------------*/
+        public static void ThrowInvalididCustomerIdException(int id)
+        {
+            throw new CustomersExecptions($"El ID del cliente '{id}' no es válido.\"");
+        }
+    }
+}
