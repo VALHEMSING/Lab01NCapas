adaac2958c5bcf3a7ddd1c7d6a7a73235daa1356
Julian David Rivera Quintero <juli.rivera.quintero@gmail.com>
2024-07-24 18:11:48 -0500

    feat(back-end): Add Interface ICustomerService.cs and into the Task

---
+++ Orders/SLC/ICustomerService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Entities.Models;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SLC
+{
+    public interface ICustomerService
+    {
+        Task<ActionResult<Customer>> CreateAsync([FromBody] Customer toCreate);
+        Task<ActionResult> DeleteAsync(int id);
+    }
+}
