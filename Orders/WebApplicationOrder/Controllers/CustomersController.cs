c15698297200451088ebaf0dc0de727bfa74fcd1
Julian <juli.rivera.quintero@gmail.com>
2024-07-31 17:56:22 -0500

    feat(Back-end): Add view Customer to customer list and create customer

--- Orders/WebApplicationOrder/Controllers/CustomersController.cs
+++ Orders/WebApplicationOrder/Controllers/CustomersController.cs
@@ -1,11 +1,11 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Entities.Models;
+using Microsoft.AspNetCore.Mvc;
 using ProxyServer;
 
 namespace WebApplicationOrder.Controllers
 {
-    public class CustomersController : Controller 
+    public class CustomersController : Controller
     {
-
         //campo
         private readonly CustomerProxy _proxy;
 
@@ -14,10 +14,70 @@ namespace WebApplicationOrder.Controllers
         {
             this._proxy = new CustomerProxy();
         }
-        public async Task<IActionResult>Index()
+
+        public async Task<IActionResult> Index()
         {
             var customers = await _proxy.GetAllAsync();
             return View(customers);
         }
+
+        /*
+         * Create
+        */
+        //GET: Customer/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,FirstName,LastName,City,Country,Phone")] Customer customer)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    var result = await _proxy.CreateAsync(customer);
+                    if (result == null)
+                    {
+                        return RedirectToAction("Error", new { message = "El cliente con el mismo nombre y aprellido ya existe." });
+                    }
+                    return RedirectToAction(nameof(Index));
+                }
+                catch (Exception ex)
+                {
+                    return RedirectToAction("Error", new { message = ex.Message });
+                }
+            }
+            return View(customer);
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     }
 }
